63e8cba8fcb24aa9f8d6bee0602bbbec964ce126
Kevin Evans <hello@kevinevans.name>
2016-06-11 11:55:59 -0700

    added shit for 0xA9
    should prob use redux before I get too far into this.

--- src/state/handlers/postlogin.js
+++ src/state/handlers/postlogin.js
@@ -1,2 +1,18 @@
 export const register = (registry, state) => {
+    registry.registerPacket(0xA9, (packet) => {
+        const characterCount = packet.getByte(3);
+        const characters = [];
+        console.log(`there are ${characterCount} character slots to loop over`);
+
+        for(let i = 0; i < characterCount; i++) {
+            let position = 4;
+            characters.push({
+                name: packet.getString(position, 30)
+            });
+
+            position += 60;
+        }
+
+        console.log(characters);
+    });
 };
