3183420fb2947996df7915be6684bbf27e23e698
lamo2k123 <lamo2k123@gmail.com>
2016-12-13 21:50:15 +0300

    merge upstream origin& move App component

--- src/client/components/application/index.jsx
+++ src/client/components/application/index.jsx
@@ -1,20 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-// import { bindActionCreators } from 'redux';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import LoginComponent from './../../ui/components/login';
 import PostLoginComponent from './../../ui/components/postlogin';
 import World from './../../ui/components/world';
 // import MainScene from './scene';
 
-// import * as loginActionCreators from '../../state/login/actions';
+import * as loginActionCreators from '../../state/login/actions';
+import * as networkActionCreators from '../../state/network/actions';
 
 import Intro from 'component/intro'
 
 import style from './style'
 
-class AppComponent
-    extends Component
-{
+class Application extends Component {
+
     static propTypes = {
         handlers: PropTypes.object.isRequired
     };
@@ -60,6 +60,9 @@ class AppComponent
 }
 
 export default connect(
-    store => ({login: store.login})
-    //dispatch => ({actions: bindActionCreators(loginActionCreators, dispatch)})
-)(AppComponent);
+        store => ({login: store.login, network: store.network}),
+        dispatch => ({
+            loginActions: bindActionCreators(loginActionCreators, dispatch),
+            networkActions: bindActionCreators(networkActionCreators, dispatch)
+        })
+)(Application);
