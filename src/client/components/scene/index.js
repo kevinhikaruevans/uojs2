938d3f46664ccb34f794caf855e06101fd98a0fb
lamo2k123 <lamo2k123@gmail.com>
2017-02-06 23:20:55 +0300

    test

--- src/client/components/scene/index.js
+++ src/client/components/scene/index.js
@@ -35,12 +35,59 @@ class Scene extends Component {
         // more work is needed to align the 0.2 to the angle that the original client uses.
         this.worldRotation = new THREE.Euler(0, 0, -1 * Math.PI / 4);
     }
+
+    componentDidMount() {
+        const renderer = new THREE.WebGLRenderer({
+            canvas : this.refs.container,
+            antialias : true
+        });
+        renderer.setClearColor(0xf0f0f0);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+        const scene = new THREE.Scene();
+
+        scene.add(this.axisHelper);
+
+        renderer.render(scene, this.camera)
+        // this.refs.container.appendChild(canvas.domElement);
+    }
+
+    get camera() {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        const aspect = width / height;
+        const viewportSize = 16;
+
+        const result = new THREE.OrthographicCamera(-1 * viewportSize * aspect, viewportSize * aspect, viewportSize, -1 * viewportSize, -100, 100);
+
+        // result.setRotationFromEuler(this.cameraRotation);
+        result.position.set(this.cameraPosition.x, this.cameraPosition.y, this.cameraPosition.z)
+        // console.log(result.getWorldDirection(this.cameraPosition), this.cameraPosition);
+
+        return result;
+    }
+
+    get axisHelper() {
+        return new THREE.AxisHelper(10);
+    }
+
+
+    mapTile = () => {
+        const geometry = new THREE.PlaneBufferGeometry();
+
+        return new THREE.Mesh(geometry, material);
+    };
+
     render() {
         const width = window.innerWidth;
         const height = window.innerHeight;
         const aspect = width / height;
         const viewportSize = 16;
 
+        return <canvas ref="container" />
+/*
         return (
             <React3 alpha
                 antialias={true}
@@ -66,6 +113,7 @@ class Scene extends Component {
                 </scene>
             </React3>
         );
+*/
     }
 }
 
